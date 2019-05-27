5dccea3e91a1280acd336e3e5391311c2375b762
Uros Ciric <ciricbgd@gmail.com>
2019-05-27 08:24:38 +0200

    ajax to html began

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 $(document).ready(function () {
 
-    //Kartice
-    var karticeDOM = $('#slike');
-    var kartice = [];
+    //Language setting
+    var lang = $('html').prop('lang');
+    if(lang=="en"){lang="eng"}else{lang="srb"};
 
-    function karticeToHtml() {
-        kartice.forEach(function (kartica, i) {
+    //JSON to HTML
+    var naslov = $('[data-input="naslov"]');
+    var podnaslov = $('[data-input="podnaslov"]');
+    var dugme = $('[data-input="dugme"]');
+    var galerijanaslov = $('[data-input="galerijanaslov"]');
+    var drustvenemreze = $('[data-input="drustvenemreze"]');
+    var adresa = $('[data-input="adresa"]');
+    var rezervacija = $('[data-input="rezervacija"]');
+    var telefon = $('[data-input="telefon"]');
+
+
+
+
+    var galerijaDOM = $('#slike');
+    var karticeDOM = $('#cards');
+    var kartice = [];
+    var galerija = [];
+
+    function jsonToHtml(j){
+       naslov.html(j.naslov[lang]);
+       podnaslov.html(j.podnaslov[lang]);
+       dugme.html(j.dugme[lang]);
+       galerijanaslov.html(j.galerijanaslov[lang]);
+       drustvenemreze.html(j.drustvenemreze[lang]);
+       adresa.html(j.adresa[lang]);
+       rezervacija.html(j.rezervacija[lang]);
+       telefon.html(j.telefon);
+    }
+
+    function galerijaToHtml() {
+        galerija.forEach(function (kartica, i) {
 
             var brojVidljivihSlikauGaleriji = 11;
 
-            var karticaTemplate = `
+            var galerijaTemplate = `
                     ${
                         Object.keys(kartica.slike).map(i => (
                             `<a ${i > brojVidljivihSlikauGaleriji ? `style="display:none"` : ``} href="./Slike/${kartica.slike[i].velika}" data-lightbox="galerija-${kartica.redniBroj}"><img src="./Slike/${kartica.slike[i].mala}" alt="${kartica.slike[i].alt ? kartica.slike[i].alt : kartica.slike[i].velika}" /></a>`
@@ -18,20 +47,49 @@ $(document).ready(function () {
             `;
 
 
-            karticeDOM.append(karticaTemplate);
+            galerijaDOM.append(galerijaTemplate);
 
         });
 
 
     }
 
-    function jsonToKartice(karticeArray) {
+
+
+    function jsonToKartice(kartice){
+        kartice.forEach(function (kartica, i) {
+
+            var karticaTemplate = `
+                    ${
+                        Object.keys(kartica).map(i => (
+                            `
+                            <div class="card">
+                                <div class="cardText">
+                                    <p>${kartica.tekst[lang]}</p>
+                                </div>
+                                <div class="cardImage">
+                                    <img src="./Slike/${kartica.slika.putanja}" alt="${kartica.slika.alt? "":kartica.slika.alt }" width="100%">
+                                </div>
+                            </div>
+                            `
+                        )).join("")
+                     }
+            `;
+
+
+            karticeDOM.append(karticaTemplate);
+
+        });
+    }
+
+
+    function jsonTogalerija(galerijaArray) {
         // Sortiranje po rednom broju
-        karticeArray.sort(function (a, b) {
+        galerijaArray.sort(function (a, b) {
             return a - b
         });
-        kartice = karticeArray;
-        karticeToHtml();
+        galerija = galerijaArray;
+        galerijaToHtml();
     }
 
 
@@ -43,8 +101,10 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
 
-            //Kartice
+            //galerija
+            jsonToHtml(data);
             jsonToKartice(data.kartice);
+            jsonTogalerija(data.galerija);
         }
     });
 
